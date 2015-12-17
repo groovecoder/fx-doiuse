40a1d220914671a9c15f7968d29c20ad9a16b265
groovecoder <luke.crouch@gmail.com>
2015-12-17 10:10:49 -0600

    use start child_process code for doiuse

--- data/doiuse-script.js
+++ data/doiuse-script.js
@@ -1,2 +1,14 @@
-if (document.body) document.body.style.border = '5px solid red';
-console.log("document.styleSheets: " + document.styleSheets);
+// Style border green to verify content script is running
+if (document.body) document.body.style.border = '5px solid green';
+
+
+for (var i=0; i < document.styleSheets.length; i++) {
+  var styleSheet = document.styleSheets[i];
+  // TODO: get the real stylesheet content from styleSheet
+  var styleSheetContent = ".disabled{cursor:not-allowed;}";
+
+  // Send the stylesheet content to add-on script to call doiuse process
+  self.port.emit("styleSheetContent", styleSheetContent);
+}
+
+// TODO: maybe receive doiuse output via self.port.on and display it?
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+var child_process = require("sdk/system/child_process");
 var tabs = require("sdk/tabs");
 var url = require("sdk/url");
 
 const DEV_DOMAINS = [
   'developer-local.allizom.org',
   'developer.allizom.org'
-]
+];
   
 
 tabs.on("ready", init);
@@ -13,8 +14,24 @@ function init(tab) {
   var tabURL = url.URL(tab.url);
   
   if (DEV_DOMAINS.indexOf(tabURL.hostname) > -1) {
-    tab.attach({
+    var tabWorker = tab.attach({
       contentScriptFile: "./doiuse-script.js"
+    });
+
+    // Receive styleSheetContent messages from content script
+    tabWorker.port.on("styleSheetContent", function(styleSheetContent) {
+      // Spawn the doiuse process
+      var doiuse = child_process.spawn("doiuse");
+
+      // Attach stdout data handler to doiuse process
+      doiuse.stdout.on('data', function(data) {
+        // TODO: show the doiuse output somehow - maybe throw it back to the content script?
+        alert(data);
+      });
+
+      // Write the stylesheet content to the doiuse process
+      doiuse.stdin.setEncoding('utf-8');
+      doiuse.stdin.write(styleSheetContent);
     });
   }
 }
