6f49b533d5f4f38fd3eb3a52e15df55ce9b7c5ea
groovecoder <luke.crouch@gmail.com>
2016-01-04 15:32:32 -0600

    proper XHR for doiuse.com

--- data/doiuse-script.js
+++ data/doiuse-script.js
@@ -7,33 +7,36 @@ self.port.on("doiuseOutput", function (doiuseOutput) {
 });
 
 for (var i=0; i < document.styleSheets.length; i++) {
-  var styleSheetHref = document.styleSheets[i].href
-  styleSheetXHR = new XMLHttpRequest();
+  var styleSheet = document.styleSheets[i];
+  if (styleSheet.href) {
+    styleSheetXHR = new XMLHttpRequest();
 
-  styleSheetXHR.onreadystatechange = function () {
-    if (styleSheetXHR.readyState === XMLHttpRequest.DONE) {
-      if (styleSheetXHR.status === 200) {
-        var styleSheetContent = styleSheetXHR.responseText;
-        var doiuseURL = 'https://www.doiuse.com/';
-        var doiuseXHR = new XMLHttpRequest();
-        doiuseXHR.open('POST', doiuseURL);
+    styleSheetXHR.onreadystatechange = function () {
+      if (styleSheetXHR.readyState === XMLHttpRequest.DONE) {
+        if (styleSheetXHR.status === 200) {
+          var styleSheetContent = styleSheetXHR.responseText;
+          var doiuseURL = 'http://127.0.0.1:3000';
+          var doiuseXHR = new XMLHttpRequest();
+          doiuseXHR.open('POST', doiuseURL);
 
-        doiuseXHR.onreadystatechange = function () {
-          if (doiuseXHR.readyState === XMLHttpRequest.DONE) {
-            if (doiuseXHR.status === 200) {
-              var doiuseOutput = doiuseXHR.responseText;
-              console.log(doiuseOutput);
+          doiuseXHR.onreadystatechange = function () {
+            if (doiuseXHR.readyState === XMLHttpRequest.DONE) {
+              if (doiuseXHR.status === 200) {
+                var doiuseOutput = doiuseXHR.responseText;
+                console.log(doiuseOutput);
+              }
             }
-          }
-        };
+          };
 
-        if (styleSheetContent) {
-          doiuseXHR.send('css=' + styleSheetContent);
+          if (styleSheetContent) {
+            doiuseXHR.send('{"css":' + JSON.stringify(styleSheetContent) + '}');
+          }
         }
       }
-    }
-  };
+    };
 
-  styleSheetXHR.open('GET', styleSheetHref);
-  styleSheetXHR.send();
+    // Don't try to GET <style> stylesheets
+    styleSheetXHR.open('GET', styleSheet.href);
+    styleSheetXHR.send();
+  }
 }
